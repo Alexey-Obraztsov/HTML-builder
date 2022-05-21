c4572de15edd25c4c140135c38704c359d646a82
Alexey-Obraztsov <alexlovi@mail.ru>
2022-05-21 21:13:36 +0300

    feat: add solution for 02-write-file task

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,4 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+const {stdin, stdout}=process;
 
+const link = path.join(__dirname, 'text.txt');
 
-
+const output = fs.createWriteStream(link);
+stdout.write('Write something>\n');
+const exit=()=>process.exit(stdout.write('Well done!'));
+process.on('SIGINT',()=>exit());
+stdin.on('data',a=>a.toString().slice(0,4)!='exit'?output.write(a):exit());
 
